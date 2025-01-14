6ac4bf693698425a113b056b5a8e862da3d2b429
billychl1 <71437290+billychl1@users.noreply.github.com>
2025-01-14 18:59:43 +0100

    Update App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -6,13 +6,33 @@ export default {
   components: {
     Activities
   },
+  data() {
+    return {
+      searchQuery: ""
+    };
+  },
+  methods: {
+    async searchActivities() {
+      const response = await fetch(`http://localhost:8080/activities/search?title=${this.searchQuery}`, {
+        method: "GET",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const activities = await response.json();
+      this.$refs.activities.updateActivities(activities);
+    }
+  }
 }
 </script>
 
 <template>
   <div id="app">
     <h1>Activities</h1>
-    <Activities :activities="activities" />
+    <input type="text" v-model="searchQuery" placeholder="Search activities by title..." @input="searchActivities" />
+    <button @click="searchActivities">Search</button>
+    <Activities ref="activities" />
   </div>
 </template>
 
